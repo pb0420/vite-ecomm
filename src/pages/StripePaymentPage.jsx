6dd39ddc2f5c6e0f790e3fda51e1a539a260ffc8
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 02:45:35 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
 import {loadStripe} from '@stripe/stripe-js';
-import {Elements,PaymentElement} from '@stripe/react-stripe-js';
+import {Elements,PaymentElement, CardForm} from '@stripe/react-stripe-js';
 import { useCart } from '@/contexts/CartContext';
 import {
   BrowserRouter as Router,
@@ -36,9 +36,13 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
   return (
    <>
     
-  <Elements options={{clientSecret:'pi_3RTkUbA3GELNlHOQ0c7fMgE3_secret_PiJo0Y5rAWZ3NK2gwLaz8ZBiy'}} stripe={loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql")} >   
+  {/* <Elements options={{clientSecret:'pi_3RTkUbA3GELNlHOQ0c7fMgE3_secret_PiJo0Y5rAWZ3NK2gwLaz8ZBiy'}} stripe={loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql")} >   
        <PaymentElement />
-    </Elements> 
+    </Elements>  */}
+
+      <Elements>
+          <CardForm fontSize={elementFontSize} />
+        </Elements>
 
      
     </>
