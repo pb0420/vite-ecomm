a7785864f286d4f5b88da0a455c757ff38eae00b
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 19:16:11 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -64,7 +64,7 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
     })
       .then((res) => res.json())
       .then((data) => data.clientSecret);
-      .then(() => setStripeCS(true))
+      .then(() => setStripeCS(true));
   }, []);
 
   const appearance = {
