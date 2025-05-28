bee7d995e6da02b9e8fd7fb8ebb2764fd05c3407
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 15:50:24 +0930

    Updated CheckoutPage.jsx

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -141,7 +141,7 @@ const CheckoutPage = () => {
               deliveryDetails={deliveryDetails} 
           /> */}
           <Button disabled={!user || !termsAccepted}> Proceed to Payment &nbsp;<CreditCard /></Button>
-          <p style={{ size:'1px'}}>Secure payment powered by Stripe</p>
+          <p style={{ 'text-size':'1px'}}>Secure payment powered by Stripe</p>
           
         </div>
       </motion.div>
