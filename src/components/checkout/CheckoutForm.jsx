a31a395ca5ec4c935846935b725952c27e63939e
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 15:28:50 +0930

    Updated CheckoutForm.jsx

--- src/components/checkout/CheckoutForm.jsx
+++ src/components/checkout/CheckoutForm.jsx
@@ -4,7 +4,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox';
 import { Link } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 import AddressSelector from '@/components/checkout/AddressSelector';
