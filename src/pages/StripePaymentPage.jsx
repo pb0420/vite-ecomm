4a2e3b2078174ce4154e788c4232435f4674ceb3
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 17:34:39 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -54,7 +54,7 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
     return fetch('https://bcbxcnxutotjzmdjeyde.supabase.co/functions/v1/create-checkout-session', {
       headers:{
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjYnhjbnh1dG90anptZGpleWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjIwODksImV4cCI6MjA2MjAzODA4OX0.sMIn31DXRvBpQsxYZV2nn1lKqdEkEk2S0jvdve2yACY'
-      }
+      },
       method: 'POST',
       data: {
         productIds:[1,2,3]
