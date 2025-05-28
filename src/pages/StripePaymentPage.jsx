2d373d21348a292b2f1855361847fc0fba397c51
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 22:17:03 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -13,6 +13,7 @@ import StripeCheckoutForm from '@/components/checkout/StripeCheckoutForm';
 
 const stripePromise = loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql");
 
+
 const Return = () => {
   const [status, setStatus] = useState(null);
   const [customerEmail, setCustomerEmail] = useState('');
@@ -71,6 +72,21 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
   }
   // }, []);
 
+  useEffect(() => {
+    fetch('https://bcbxcnxutotjzmdjeyde.supabase.co/functions/v1/create-checkout-session', {
+      headers:{
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjYnhjbnh1dG90anptZGpleWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjIwODksImV4cCI6MjA2MjAzODA4OX0.sMIn31DXRvBpQsxYZV2nn1lKqdEkEk2S0jvdve2yACY'
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        productIds:productIds
+      })
+    }).then((res) => res.json()).then((data) => setStripeCS(data.clientSecret));
+      
+    
+  }, [])
+
+  
   const appearance = {
     theme: 'stripe',
   };
@@ -78,9 +94,9 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
   return (
    <div>
     
-    <Elements stripe={stripePromise} options={{fetchClientSecret}} > 
+     { stripeCS && (<Elements stripe={stripePromise} options={{fetchClientSecret}} > 
        <StripeCheckoutForm />
-    </Elements>
+    </Elements>) }
     </div>
   )
   
