28ecd4f8325a0ef7d53515fea45c72e3366a41c7
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 23:19:38 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -12,64 +12,13 @@ import StripeCheckoutForm from '@/components/checkout/StripeCheckoutForm';
 
 
 
-const Return = () => {
-  const [status, setStatus] = useState(null);
-  const [customerEmail, setCustomerEmail] = useState('');
-
-  useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const sessionId = urlParams.get('session_id');
-
-    fetch(`/session-status?session_id=${sessionId}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setStatus(data.status);
-      });
-  }, []);
-
-  if (status === 'open') {
-    return (
-      <Navigate to="/checkout" />
-    )
-  }
-
-  if (status === 'complete') {
-    return (
-      <section id="success">
-        <p>
-Success. Redirecting...
-        </p>
-      </section>
-    )
-  }
-
-  return null;
-}
-
 const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
 
   const { cart, getCartTotal, clearCart } = useCart();
   const productIds = cart.map(item => item.id);
   const [stripeCS, setStripeCS] = useState(false);
   const stripePromise = loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql");
-  // const promise = useMemo(() => {
-  const fetchClientSecret = async () => {
-    const csData = await fetch('https://bcbxcnxutotjzmdjeyde.supabase.co/functions/v1/create-checkout-session', {
-      headers:{
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjYnhjbnh1dG90anptZGpleWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjIwODksImV4cCI6MjA2MjAzODA4OX0.sMIn31DXRvBpQsxYZV2nn1lKqdEkEk2S0jvdve2yACY'
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        productIds:productIds
-      })
-    })
-      .then((res) => res.json())
-      .then((data) => data.clientSecret);
-    console.log(csData,'cscscs');
-    return csData;
-  }
-  // }, []);
+
 
   useEffect(() => {
     fetch('https://bcbxcnxutotjzmdjeyde.supabase.co/functions/v1/create-checkout-session', {
