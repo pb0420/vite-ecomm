576e169d9db7a8698fc2464975bb43202d3c906d
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 18:11:21 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -57,7 +57,7 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjYnhjbnh1dG90anptZGpleWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjIwODksImV4cCI6MjA2MjAzODA4OX0.sMIn31DXRvBpQsxYZV2nn1lKqdEkEk2S0jvdve2yACY'
       },
       method: 'POST',
-      data: {
+      body: {
         productIds:productIds
       }
     })
