c3e0d655e8698f412cc87f9d044c29ed20c5016a
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 02:46:42 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -41,7 +41,7 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
     </Elements>  */}
 
       <Elements>
-          <CardForm fontSize={elementFontSize} />
+          <CardForm />
         </Elements>
 
      
