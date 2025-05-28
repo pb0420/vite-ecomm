92b1a89e2fa4de3bf80f5bd3e8b121c5552c5df6
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 20:59:10 +0930

    Updated StripeCheckoutForm.jsx

--- src/components/checkout/StripeCheckoutForm.jsx
+++ src/components/checkout/StripeCheckoutForm.jsx
@@ -4,8 +4,51 @@ import {
   useCheckout,
 } from '@stripe/react-stripe-js';
 
-const StripeCheckoutForm = () => {
- 
+const validateEmail = async (email, checkout) => {
+  const updateResult = await checkout.updateEmail(email);
+  const isValid = updateResult.type !== "error";
+
+  return { isValid, message: !isValid ? updateResult.error.message : null };
+}
+
+const EmailInput = ({ email, setEmail, error, setError }) => {
+  const checkout = useCheckout();
+
+  const handleBlur = async () => {
+    if (!email) {
+      return;
+    }
+
+    const { isValid, message } = await validateEmail(email, checkout);
+    if (!isValid) {
+      setError(message);
+    }
+  };
+
+  const handleChange = (e) => {
+    setError(null);
+    setEmail(e.target.value);
+  };
+
+  return (
+    <>
+      <label>
+        Email
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder="you@example.com"
+        />
+      </label>
+      {error && <div id="email-errors">{error}</div>}
+    </>
+  );
+};
+
+const CheckoutForm = () => {
   const checkout = useCheckout();
 
   const [email, setEmail] = useState('');
@@ -16,8 +59,16 @@ const StripeCheckoutForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    setIsLoading(true);
 
- 
+    const { isValid, message } = await validateEmail(email, checkout);
+    if (!isValid) {
+      setEmailError(message);
+      setMessage(message);
+      setIsLoading(false);
+      return;
+    }
+
     const confirmResult = await checkout.confirm();
 
     // This point will only be reached if there is an immediate error when
@@ -34,6 +85,12 @@ const StripeCheckoutForm = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <EmailInput
+        email={email}
+        setEmail={setEmail}
+        error={emailError}
+        setError={setEmailError}
+      />
       <h4>Payment</h4>
       <PaymentElement id="payment-element" />
       <button disabled={isLoading} id="submit">
