cb6c9a3f7031cb59f12349a7d6304c0600ae3f8f
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 23:39:48 +0930

    Updated LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,12 +8,12 @@ const LoginPage = () => {
   //   <div className="container px-4 py-8 mx-auto md:px-6">
   //     <CustomerLoginFormCard />
   //   </div>
-  // );
 
 
   <Elements options={{clientSecret:'pi_3RTkUbA3GELNlHOQ0c7fMgE3_secret_PiJo0Y5rAWZ3NK2gwLaz8ZBiy'}} stripe={loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql")} >   
       <PaymentElement />
     </Elements> 
+  )
 };
 
 export default LoginPage;
