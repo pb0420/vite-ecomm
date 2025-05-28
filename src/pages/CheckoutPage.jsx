c2fb6b6ee3eba39b61fd5a03bf3e2104c5cb2612
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 16:53:52 +0930

    Updated CheckoutPage.jsx

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -140,7 +140,7 @@ const CheckoutPage = () => {
             </Label>
           </div>
 
-          <Button disabled={!user || !termsAccepted} onClick={setShowStripePaymentSection(true)}}> Proceed to Payment &nbsp; <CreditCard /></Button>
+          <Button disabled={!user || !termsAccepted} onClick={setShowStripePaymentSection(true)}> Proceed to Payment &nbsp; <CreditCard /></Button>
           <p style={{ marginTop:'-25px',fontSize:'8px'}}>Secure payment powered by Stripe</p>
         </div>
       </motion.div>
