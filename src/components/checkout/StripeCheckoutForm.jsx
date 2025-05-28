68d9e0943a966af92b9e33189c947f05b13e0c2c
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-28 17:30:19 +0930

    Updated StripeCheckoutForm.jsx

---
+++ src/components/checkout/StripeCheckoutForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import {
+  PaymentElement,
+  useCheckout,
+} from '@stripe/react-stripe-js';
+
+
+const StripeCheckoutForm = () => {
+  const checkout = useCheckout();
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+ 
+    const confirmResult = await checkout.confirm();
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (confirmResult.type === 'error') {
+      setMessage(confirmResult.error.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Payment</h4>
+      <PaymentElement id="payment-element" />
+      <button disabled={isLoading} id="submit">
+        {isLoading ? (
+          <div className="spinner"></div>
+        ) : (
+          `Pay ${checkout.total.total.amount} now`
+        )}
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
+  );
+}
+
+export default StripeCheckoutForm;
