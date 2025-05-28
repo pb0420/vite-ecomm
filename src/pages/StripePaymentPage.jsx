96fcfbc469cce1bb38cf5b9e951830a9ee7673a1
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 02:47:38 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -42,7 +42,7 @@ const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
 
       <Elements>
           <PaymentElement />
-        </Elements> 
+        </Elements>  
 
      
     </>
