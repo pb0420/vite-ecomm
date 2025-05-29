b8b57475f91fc61e9ebc695a7bbdb0a5a41321d6
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 14:07:20 +0930

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,10 @@ const App = () => {
                     <Route path="/admin" element={<AdminPage />} />
                     <Route path="/privacy" element={<PrivacyPage />} />
                     <Route path="/terms" element={<TermsPage />} />
-                    <Route path="/stripe-payment" element={  <Elements options = {{mode:'payment', currency:'aud', amount:1100}} stripe={stripePromise} > 
+                    <Route path="/stripe-payment" element={  <Elements options = {{mode:'payment', currency:'aud', amount:1100, layout: {
+    type: 'tabs',
+    defaultCollapsed: false,
+  } }} stripe={stripePromise} > 
   <StripeCheckoutForm /> 
   </Elements>} />
                   </Routes>
