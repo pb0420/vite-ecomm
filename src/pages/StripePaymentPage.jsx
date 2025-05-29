f0d3ffd717c4ac257e8c10dbcd764b3ae7c3c919
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 13:14:48 +0930

    Updated StripePaymentPage.jsx

--- src/pages/StripePaymentPage.jsx
+++ src/pages/StripePaymentPage.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect, useMemo } from "react";
-import {loadStripe} from '@stripe/stripe-js';
-import {Elements, PaymentElement, useStripe, useElements} from '@stripe/react-stripe-js';
-import { useCart } from '@/contexts/CartContext';
 import {
   BrowserRouter as Router,
   Route,
@@ -9,44 +6,14 @@ import {
   Navigate,
   useNavigate
 } from "react-router-dom";
+import { StripeCheckoutForm } from '@/components/checkout/StripeCheckoutForm';
+  
 
-const stripePromise = loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql");
 const StripePaymentPage = ({ customerDetails, deliveryDetails }) => {
-
-  const stripe = useStripe();
-  const elements = useElements();
-  const navigate = useNavigate();
-  const { cart, getCartTotal, clearCart } = useCart();
-  const productIds = cart.map(item => item.id);
-  const [stripeCS, setStripeCS] = useState(false);
-
-
-  // useEffect(() => {
-  //   fetch('https://bcbxcnxutotjzmdjeyde.supabase.co/functions/v1/create-checkout-session', {
-  //     headers:{
-  //       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJjYnhjbnh1dG90anptZGpleWRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NjIwODksImV4cCI6MjA2MjAzODA4OX0.sMIn31DXRvBpQsxYZV2nn1lKqdEkEk2S0jvdve2yACY'
-  //     },
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       productIds:productIds
-  //     })
-  //   }).then((res) => res.json()).then((data) => setStripeCS(data.clientSecret));
-    
-  // }, [])
-
 
   return (
 
-<Elements options = {{ mode:'payment', currency:'usd', amount:1999 , appearance : {
-    theme: 'stripe',
-  }}} stripe={stripePromise} >
-
-       <form>
-         here goes the PE
-          <PaymentElement />
-         
-      </form> 
-    </Elements>
+<StripeCheckoutForm />
      
   )
   
