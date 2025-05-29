197c883dd4d3d028ef6cbd8f6ec4729510a869ee
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 13:28:37 +0930

    Updated StripeCheckoutForm.jsx

--- src/components/checkout/StripeCheckoutForm.jsx
+++ src/components/checkout/StripeCheckoutForm.jsx
@@ -13,7 +13,7 @@ import {
 const StripeCheckoutWrapper = () => {
   
 
-     <Elements options = {{ mode:'payment', currency:'usd', amount:1999 , appearance : {
+     <Elements optionss = {{ mode:'payment', currency:'usd', amount:1999 , appearance : {
     theme: 'stripe',
   }}} stripe={stripePromise} > 
   <StripeCheckoutForm />
