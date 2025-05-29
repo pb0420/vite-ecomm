bd06cef782385edb2983da4fec6c876fc9644788
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 17:35:26 +0930

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 
 import React from "react";
 import { render } from "react-dom";
-import App from "./App.jsx";
+import App from "@/App";
+import '@/index.css';
 
 const container = document.getElementById("root");
 render(
@@ -10,3 +11,14 @@ render(
   </React.StrictMode>,
   container
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from '@/App';
+// import '@/index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
