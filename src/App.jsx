9f031e0828e654d5c075e1a565940d0d5770c320
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 12:18:55 +0930

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ const App = () => {
                     <Route path="/admin" element={<AdminPage />} />
                     <Route path="/privacy" element={<PrivacyPage />} />
                     <Route path="/terms" element={<TermsPage />} />
-                    <Route path="/stripe-payment" element={ <Elements options = {{ mode:'payment', currency:'usd' amount:1999}} stripe={stripePromise} ><StripePaymentPage /> </Elements> } /> 
+                    <Route path="/stripe-payment" element={ <Elements options = {{ mode:'payment', currency:'usd', amount:1999}} stripe={stripePromise} ><StripePaymentPage /> </Elements> } /> 
                   </Routes>
                 </AnimatePresence>
               </main>
