b188696489af43f3d553e227863efd7c92c40659
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-29 13:07:29 +0930

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 
-import {loadStripe} from '@stripe/stripe-js';
-import {Elements} from '@stripe/react-stripe-js';
-
 // Contexts
 import { CartProvider } from '@/contexts/CartContext';
 import { OrderProvider } from '@/contexts/OrderContext';
@@ -33,7 +30,7 @@ import StorePickupPage from '@/pages/StorePickupPage';
 import PrivacyPage from '@/pages/PrivacyPage';
 import TermsPage from '@/pages/TermsPage';
 import StripePaymentPage from '@/pages/StripePaymentPage';
-const stripePromise = loadStripe("pk_test_L1f0e3XAzjsG7jtp4uN7L9ql");
+
 const App = () => {
   return (
     <AuthProvider>
@@ -60,9 +57,7 @@ const App = () => {
                     <Route path="/admin" element={<AdminPage />} />
                     <Route path="/privacy" element={<PrivacyPage />} />
                     <Route path="/terms" element={<TermsPage />} />
-                    <Route path="/stripe-payment" element={ <Elements options = {{ mode:'payment', currency:'usd', amount:1999 , appearance : {
-    theme: 'stripe',
-  }}} stripe={stripePromise} ><StripePaymentPage /> </Elements> } /> 
+                    <Route path="/stripe-payment" element={ <StripePaymentPage /> } /> 
                   </Routes>
                 </AnimatePresence>
               </main>
