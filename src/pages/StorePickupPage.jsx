292af734f452777f8929fc7b38481faac678b282
pb0420 <pranjalbatra22@yahoo.co.in>
2025-05-30 20:38:28 +0930

    Updated StorePickupPage.jsx

--- src/pages/StorePickupPage.jsx
+++ src/pages/StorePickupPage.jsx
@@ -20,6 +20,8 @@ import { cn } from '@/lib/utils';
 import { Link } from 'react-router-dom';
 import AddressSelector from '@/components/checkout/AddressSelector';
 import { formatCurrency } from '@/lib/utils';
+import { CalendarPicker } from '@/components/ui/calendar';
+
 
 const generateTimeSlots = () => {
   const slots = [];
